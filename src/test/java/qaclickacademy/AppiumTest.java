fb94beafcab57ff5f490cb343a01d64ad47c38bd
ananthakalyani <navyakalyani5@gmail.com>
2022-06-09 00:29:54 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+    @Test
+    public void NativeAPAndriod() {
+
+        System.out.println("NativeAPAndriod");
+    }
+
+    @Test
+    public void IOSApp() {
+        System.out.println("IOSApp");
+
+    }
+}
